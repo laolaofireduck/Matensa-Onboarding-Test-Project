eb008dedb48607b1fdb734a1479f93c1131cadb8
Leen Allaf <leenallaf98@gmail.com>
2024-05-04 09:52:15 +0300

    chore: add user entity

--- src/Ewallet.Core/Domain/Users/User.cs
+++ src/Ewallet.Core/Domain/Users/User.cs
@@ -1,10 +1,42 @@
-﻿namespace Ewallet.Core.Domain.Users;
+﻿using Ewallet.SharedKernel;
 
-public class User
+namespace Ewallet.Core.Domain.Users;
+
+public class User : AggregateRoot<UserId, Guid>
 {
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
+    public DateOnly DOB { get; private set; }
     public string Email { get; private set; }
     public string Password { get; private set; }
-    
+
+    private User(
+        UserId id,
+        string firstName,
+        string lastName,
+        DateOnly dob,
+        string email,
+        string password) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        DOB = dob;
+        Email = email;
+        Password = password;
+    }
+    public static User Create(
+    string firstName,
+    string lastName,
+    DateOnly dob,
+    string email,
+    string password)
+    {
+        return new(
+            UserId.CreateUnique(),
+            firstName,
+            lastName,
+            dob,
+            email,
+            password);
+    }
 }
